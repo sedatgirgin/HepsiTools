1a8ae93996b3c8b8342a6069d08444298a1d7685
sedatgirgin <sedatgirgin3509@gmail.com>
2022-05-21 12:39:54 +0300

    few

--- HepsiTools/DataAccess/ToolDbContext.cs
+++ HepsiTools/DataAccess/ToolDbContext.cs
@@ -14,13 +14,6 @@ namespace HepsiTools.DataAccess
             : base(options)
         {
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseNpgsql("User ID=okrpwbsxbljoxg;Password=057dc652a0fc8a972a889eca2f56f9cf33cf967175809767071a41be8d47c891;Server=ec2-44-194-117-205.compute-1.amazonaws.com; Port=5432;Database=d5r4cg6smcu1ba;Integrated Security=true;Pooling=true;");
-            }
-        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().HasMany(I => I.Companies).
