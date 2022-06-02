d01f6d54023de2f1476979acb75c0a3b5aeec99c
sedatgirgin <sedatgirgin3509@gmail.com>
2022-06-02 11:20:14 +0300

    sa

--- HepsiTools/Business/CompetitionBackgroundService.cs
+++ HepsiTools/Business/CompetitionBackgroundService.cs
@@ -3,15 +3,15 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Timers;
 
 namespace HepsiTools.Business
 {
-    public class CompetitionBackgroundService : BackgroundService
+    public class CompetitionBackgroundService : IHostedService, IDisposable
     {
         private readonly ICompetitionAnalsesHistoryRepository _competitionAnalsesHistoryRepository;
         private readonly ICompanyRepository _companyRepository;
         private readonly ICompetitionAnalysesRepository _competitionAnalysesRepository;
+        private Timer _timer = null;
 
         public CompetitionBackgroundService(ICompetitionAnalsesHistoryRepository competitionAnalsesHistoryRepository, ICompanyRepository companyRepository, ICompetitionAnalysesRepository competitionAnalysesRepository)
         {
@@ -19,48 +19,35 @@ namespace HepsiTools.Business
             _companyRepository = companyRepository;
             _competitionAnalysesRepository = competitionAnalysesRepository;
         }
-
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        public Task StartAsync(CancellationToken stoppingToken)
         {
-            stoppingToken.ThrowIfCancellationRequested();
-
-            SetTimer();
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,TimeSpan.FromMinutes(15));
 
             return Task.CompletedTask;
         }
 
-        public override async Task StopAsync(CancellationToken stoppingToken)
+        private void DoWork(object? state)
         {
-            await base.StopAsync(stoppingToken);
-        }
-
-        public override async Task StartAsync(CancellationToken stoppingToken)
-        {
-            await base.StartAsync(stoppingToken);
-        }
-
-        private static void SetTimer()
-        {
-            // Create a timer with a two second interval.
-            System.Timers.Timer aTimer = new System.Timers.Timer(900);
-            // Hook up the Elapsed event for the timer. 
-            aTimer.Elapsed += OnTimedEvent;
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
-        }
-
-        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
-        {
-
-            var data = _competitionAnalysesRepository.GetList(i => i.StatusType != Helper.StatusType.Cancel && );
+            var data = _competitionAnalysesRepository.GetList(i => i.StatusType != Helper.StatusType.Cancel && i.StartDate > DateTime.Now && i.EndDate < DateTime.Now);
 
             while (data.Count > 0)
             {
-
-
-
+                //html parser yaz
+                //fiyat deglştirme algorritması ekle
+                //history tablosunu doldur
             }
         }
 
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
     }
 }
