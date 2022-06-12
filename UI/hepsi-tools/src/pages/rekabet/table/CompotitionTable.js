4f693ed4563fa415be7769053283579f5471be5b
naim-yurek <naimyurek@gmail.com>
2022-06-12 12:55:41 +0300

    faet: table için header css yazıldı.

--- UI/hepsi-tools/src/pages/rekabet/table/CompotitionTable.js
+++ UI/hepsi-tools/src/pages/rekabet/table/CompotitionTable.js
@@ -1,67 +1,71 @@
 import React from 'react';
+import "./CompotitionTable.css";
+import RightCircleArrowIcon from "../../../static/assets/right-circle-arrow.png";
 
 function CompotitionTable(props) {
     return (
-        <table>
-            <tr>
-                <th>
-                    <input type="checkbox"  id="allCheck"/>
-                </th>
-                <th>
+        <div className="compotition-table-wrapper">
+            <table className="compotition-table">
+                <tr className="compotition-table-header">
+                    <th>
+                        <input type="checkbox" id="allCheck"/>
+                    </th>
+                    <th>
 
-                </th>
-                <th>
-                    #
-                </th>
-                <th>
-                    ÜRÜN ADI
-                </th>
-                <th>
-                    DURUMU
-                </th>
-                <th>
-                    GÜNCELLEME TARİHİ
-                </th>
-                <th>
-                    TEKRARLAMA
-                </th>
-                <th>
-                    REKABET TARİHİ
-                </th>
-                <th>
-                    SATIŞ FİYATI
-                </th>
-            </tr>
-            <tr>
-                <th>
-                    <input type="checkbox"  id="allCheck"/>
-                </th>
-                <th>
-                    icon
-                </th>
-                <th>
-                    1
-                </th>
-                <th>
-                    Allosaurus web app
-                </th>
-                <th>
-                    İşlemde
-                </th>
-                <th>
-                    15 Mar 2021, 12:47 PM
-                </th>
-                <th>
-                    3
-                </th>
-                <th>
-                    15 May 2021 > 15 Aug 2021
-                </th>
-                <th>
-                    100.90 TL
-                </th>
-            </tr>
-        </table>
+                    </th>
+                    <th>
+                        #
+                    </th>
+                    <th>
+                        ÜRÜN ADI
+                    </th>
+                    <th>
+                        DURUMU
+                    </th>
+                    <th>
+                        GÜNCELLEME TARİHİ
+                    </th>
+                    <th>
+                        TEKRARLAMA
+                    </th>
+                    <th>
+                        REKABET TARİHİ
+                    </th>
+                    <th>
+                        SATIŞ FİYATI
+                    </th>
+                </tr>
+                <tr>
+                    <th>
+                        <input type="checkbox" id="allCheck"/>
+                    </th>
+                    <th>
+                        <img src={RightCircleArrowIcon} alt="expanded"/>
+                    </th>
+                    <th>
+                        1
+                    </th>
+                    <th>
+                        Allosaurus web app
+                    </th>
+                    <th>
+                        İşlemde
+                    </th>
+                    <th>
+                        15 Mar 2021, 12:47 PM
+                    </th>
+                    <th>
+                        3
+                    </th>
+                    <th>
+                        15 May 2021 > 15 Aug 2021
+                    </th>
+                    <th>
+                        100.90 TL
+                    </th>
+                </tr>
+            </table>
+        </div>
     );
 }
 
